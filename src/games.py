4f016a503da1101c33da1e1de2bad0fba99ad9cf
Landarby Vincent <vincentlandarby@gmail.com>
2024-02-13 03:59:29 -0500

    games page

--- src/games.py
+++ src/games.py
@@ -36,5 +36,30 @@ def get_games():
     data = response.text
     parse_json = json.loads(data)
 
-    st.write(parse_json)
+    #Initiate empty list to store data
+    # visitor_list = []
+    # visitor_points = []
+    # home_list = []
+    # home_points = []
+    # status_list = []
+
+    #variable to get the number of games
+    num_games = parse_json['results']
+
+    if num_games == 0:
+        st.error("No games")
+    else:
+        for num in range(num_games):
+            
+            visitor_list = parse_json['response'][num]['teams']['visitors']['nickname']
+            visitor_points= parse_json['response'][num]['scores']['visitors']['points']
+            
+            home_list = parse_json['response'][num]['teams']['home']['nickname']
+            home_points = parse_json['response'][num]['scores']['home']['points']
+            
+            status = parse_json['response'][num]['status']['long']
     
+            col1, col2, col3 = st.columns(3)
+            col1.metric(visitor_list, visitor_points)
+            col2.metric(home_list, home_points)
+            col3.metric("Status", status)
